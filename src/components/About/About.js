757c26b422846b0add23d24cf5e018da8989859b
izak <izabkru88@gmail.com>
2021-06-07 16:02:54 +0100

    animations

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,15 @@
 import classes from './About.module.css';
 import img from '../../assets/images/i21.png';
 import AboutBox from './AboutBox/AboutBox';
+import Aos from 'aos';
+import React from 'react';
 
 export default function About() {
+  React.useEffect(() => {
+    Aos.init({
+      duration: 2000,
+    });
+  }, []);
   return (
     <section
       style={{
@@ -13,13 +20,15 @@ export default function About() {
       id='about'
     >
       <div className={classes.image}></div>
+      <div data-aos-offset='200' data-aos='zoom-in' className={classes.heading}>
+        <h1>
+          <span>About</span> Us
+        </h1>
+      </div>
       <div className={classes.container}>
-        <div className={classes.heading}>
-          <h1>
-            <span>About</span> Us
-          </h1>
+        <div data-aos='fade-up' data-aos-mirror='true'>
+          <AboutBox />
         </div>
-        <AboutBox />
       </div>
     </section>
   );
--- src/components/Prices/Prices.js
+++ src/components/Prices/Prices.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import Boxes from './Boxes/Boxes';
+import Aos from 'aos';
 
 import classes from './Prices.module.css';
 import Cards from './Cards/Cards';
 
 export default function Prices() {
+  React.useEffect(() => {
+    Aos.init({
+      duration: 2000,
+    });
+  }, []);
+
   return (
     <section className={classes.prices} id='prices'>
       <div className={classes.container}>
-        <div className={classes.heading}>
+        <div data-aos='zoom-in' className={classes.heading}>
           <h1>
             <span>Prices</span> And Services
           </h1>
         </div>
-        <Cards />
-        <Boxes />
+        <div data-aos='fade-up' data-aos-mirror='true'>
+          <Cards />
+        </div>
+        <div data-aos='fade-up' data-aos-mirror='true'>
+          <Boxes />
+        </div>
       </div>
     </section>
   );

9ffcc431a75ae04a67155c9a86a4282881c107f6
izak <izabkru88@gmail.com>
2021-06-08 11:52:36 +0100

    detele unused images and icons

--- src/components/Prices/Boxes/Boxes.js
+++ src/components/Prices/Boxes/Boxes.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import classes from './Boxes.module.css';
-import icon1 from '../../../assets/images/icons/polish.svg';
 
 const boxItems = [
   {
     id: 1,
     title: 'Machine Paint Correction',
-    image: icon1,
     text1: '1 Stage',
     text2: 'Small Car - £100',
     text3: 'Medium Car - £130',
@@ -15,7 +13,6 @@ const boxItems = [
   {
     id: 2,
     title: 'Machine Paint Correction',
-    image: icon1,
     text1: '2 Stage',
     text2: 'Small Car - £200',
     text3: 'Medium Car - £250',
@@ -24,7 +21,6 @@ const boxItems = [
   {
     id: 3,
     title: 'Machine Paint Correction',
-    image: icon1,
     text1: '3 Stage',
     text2: 'Small Car - £300',
     text3: 'Medium Car - £350',
@@ -37,9 +33,6 @@ export default function Boxes() {
     <div className={classes.box}>
       {boxItems.map((boxItem, index) => (
         <div className={classes.box__item} key={boxItem.id}>
-          {/* <div className={classes.icon}>
-            <img src={boxItem.image} alt='Icon' />
-          </div> */}
           <h2>{boxItem.title}</h2>
           <p>{boxItem.text1}</p>
           <p>{boxItem.text2}</p>
